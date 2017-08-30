d39a4624c7c51516b801e83778240d9d5ee117e2
yuanxuegui <yuanxuegui@163.com>
2017-08-30 13:42:23 +0800

    interceptors add listener({app, history}) run in app.start().

--- examples/index.js
+++ examples/index.js
@@ -1,18 +1,14 @@
 import { observable, action } from 'mobx'
-import humbird from '../lib/humbird'
+import humbird, { createHashHistory } from '../lib/humbird'
 import router from './router'
 import todoModel from './models/todoModel'
 
-const app = humbird()
+const history = createHashHistory()
+const app = humbird({
+    history
+})
 
 app.model(todoModel)
-
-app.models.todoModel.addTodo("read MobX tutorial");
-app.models.todoModel.addTodo("try MobX");
-app.models.todoModel.todos[0].completed = true;
-app.models.todoModel.todos[1].task = "try MobX in own project";
-app.models.todoModel.todos[0].task = "grok MobX tutorial";
-                        
 
 app.router(router)
 
